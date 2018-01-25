2c38f2196d6ed6bb43144f03646e4285fa2857cc
Stephen Thurman <thurman024@gmail.com>
2018-01-25 15:02:04 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,9 @@ def my_find(collection)
   i = 0
   return_values = []
   while i < collection.length
-    yield collection[i]
+    if yield collection[i]
+      return collection[i]
+    end
     i += 1
   end
   return_values
